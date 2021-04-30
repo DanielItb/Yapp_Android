1c4db48ada641278b82b703da9a5a371661cebcb
Dani <daniel.acosta.7e3@itb.cat>
2021-04-30 18:16:42 +0200

    Form patient 1.0

--- app/src/main/java/cat/itb/yapp/fragments/UserProfileFragment.java
+++ app/src/main/java/cat/itb/yapp/fragments/UserProfileFragment.java
@@ -8,11 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+
 import cat.itb.yapp.R;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class UserProfileFragment extends Fragment {
+    private MaterialButton birthDateButton;
+    private TextInputEditText registerDate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,30 @@ public class UserProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_user_profile, container, false);
+
+        registerDate = v.findViewById(R.id.registerDateEditText);
+
+        birthDateButton = v.findViewById(R.id.birthDateButton);
+        birthDateButton.setOnClickListener(this::datePickerCall);
+
+        return v;
+    }
+
+    public void datePickerCall(View v) {
+        datePicker();
+    }
+
+    public void datePicker() {
+        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select date: ");
+        final MaterialDatePicker<Long> picker = builder.build();
+        picker.show(getChildFragmentManager(), picker.toString());
+        if (picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+            @Override
+            public void onPositiveButtonClick(Long selection) {
+                birthDateButton.setText(String.valueOf(picker.getHeaderText()));
+            }
+        })) ;
     }
 }
