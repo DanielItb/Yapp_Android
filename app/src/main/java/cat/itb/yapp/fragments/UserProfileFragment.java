ac2dae9c50e9f524c586f484358379c9282ecb63
Dani <daniel.acosta.7e3@itb.cat>
2021-04-30 16:24:43 +0200

    Initi yapp sensual

---
+++ app/src/main/java/cat/itb/yapp/fragments/UserProfileFragment.java
@@ -0,0 +1,27 @@
+package cat.itb.yapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.yapp.R;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class UserProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+}
