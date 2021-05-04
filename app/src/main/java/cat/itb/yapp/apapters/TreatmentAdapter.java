89628bee1ec6882cacdf0e067c74a163d7ef0bb8
David Lama Pulido <david.lama.7e3@itb.cat>
2021-05-04 18:05:18 +0200

    recycler

---
+++ app/src/main/java/cat/itb/yapp/apapters/TreatmentAdapter.java
@@ -0,0 +1,4 @@
+package cat.itb.yapp.apapters;
+
+public class TreatmentAdapter {
+}
---
+++ app/src/main/java/cat/itb/yapp/fragments/TreatmentListFragment.java
@@ -0,0 +1,32 @@
+package cat.itb.yapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.yapp.R;
+
+
+public class TreatmentListFragment extends Fragment {
+    private RecyclerView recyclerView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_treatment_list, container, false);
+        recyclerView = v.findViewById(R.id.recyclerTreatment);
+
+        return v;
+    }
+}
