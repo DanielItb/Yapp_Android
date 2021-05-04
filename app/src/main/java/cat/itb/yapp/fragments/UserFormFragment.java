5d8d105bb2bd10d432cce16c2fb5e9cd55333d54
Daniel Acosta Romero <daniel.acosta.7e3@itb.cat>
2021-05-04 12:54:03 +0200

    patient form, user form, styles, strings y lo que quieras lo tengo barato oiga

---
+++ app/src/main/java/cat/itb/yapp/fragments/UserFormFragment.java
@@ -0,0 +1,37 @@
+package cat.itb.yapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import cat.itb.yapp.R;
+
+public class UserFormFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_user_form, container, false);
+
+        final String[] rolTypes = getResources().getStringArray(R.array.rol_types);
+        AutoCompleteTextView rolTypesDropDown = v.findViewById(R.id.autoCompleteRol);
+        ArrayAdapter<String> adapterRol = new ArrayAdapter<String>(requireContext(), R.layout.drop_down_types, rolTypes);
+        rolTypesDropDown.setAdapter(adapterRol);
+
+        final String[] specialistTypes = getResources().getStringArray(R.array.specialist_types);
+        AutoCompleteTextView specialistTypesDropDown = v.findViewById(R.id.autoCompleteSpecialist);
+        ArrayAdapter<String> adapterSpecialist = new ArrayAdapter<String>(requireContext(), R.layout.drop_down_types, specialistTypes);
+        specialistTypesDropDown.setAdapter(adapterSpecialist);
+        return v;
+    }
+}
