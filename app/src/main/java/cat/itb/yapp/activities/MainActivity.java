452b920553b1f941d106feca193273f746d7a562
David Lama Pulido <david.lama.7e3@itb.cat>
2021-05-05 13:36:55 +0200

    navigation :D

--- app/src/main/java/cat/itb/yapp/activities/MainActivity.java
+++ app/src/main/java/cat/itb/yapp/activities/MainActivity.java
@@ -1,9 +1,19 @@
 package cat.itb.yapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 import cat.itb.yapp.R;
 
@@ -17,8 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         activity = this;
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView navView = findViewById(R.id.nav_view);
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        final NavController navController = navHostFragment.getNavController();
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.loginFragment, R.id.mainFragment)
+                        .setOpenableLayout(drawerLayout)
+                        .build();
+
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
     }
+
 
 
     public static Activity getActivity() {
