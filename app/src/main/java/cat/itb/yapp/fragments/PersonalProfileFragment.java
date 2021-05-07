7269167ad4c8a2857967395f0fc4cd632a2966d1
Dani <daniel.acosta.7e3@itb.cat>
2021-05-07 18:07:32 +0200

    Personal profile fragment layout done

--- app/src/main/java/cat/itb/yapp/fragments/PersonalProfileFragment.java
+++ app/src/main/java/cat/itb/yapp/fragments/PersonalProfileFragment.java
@@ -17,9 +17,8 @@ public class PersonalProfileFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_personal_profile, container, false);
+        return v;
     }
 }
