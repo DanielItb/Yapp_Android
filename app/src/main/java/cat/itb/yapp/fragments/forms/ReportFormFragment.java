a6bbeb306973922473cc48e0236cf8135ffedce9
David Lama Pulido <david.lama.7e3@itb.cat>
2021-05-17 13:39:38 +0200

    reportForm

--- app/src/main/java/cat/itb/yapp/fragments/forms/ReportFormFragment.java
+++ app/src/main/java/cat/itb/yapp/fragments/forms/ReportFormFragment.java
@@ -2,6 +2,8 @@ package cat.itb.yapp.fragments.forms;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -17,6 +19,8 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import cat.itb.yapp.R;
 import cat.itb.yapp.models.report.ReportDto;
+import cat.itb.yapp.models.report.UpdateReportDto;
+import cat.itb.yapp.models.treatment.TreatmentDto;
 
 public class ReportFormFragment extends Fragment {
     private NavController navController;
@@ -24,11 +28,27 @@ public class ReportFormFragment extends Fragment {
     private MaterialButton buttonPatient, buttonSpecialist, buttonCancel, buttonSave;
     private SwitchCompat switchActive;
     private ReportDto reportDto = null;
+    private boolean editing;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         navController = NavHostFragment.findNavController(this);
+
+        final FragmentManager fragmentManager = getParentFragmentManager();
+
+        fragmentManager.setFragmentResultListener("userId", this, (requestKey, bundle) -> {
+            reportDto.setSpecialistId(String.valueOf(bundle.getLong("userId"))); //TODO Remove parse
+            String fullName = bundle.getString("fullName");
+            reportDto.setSpecialistFullName(fullName);
+            buttonSpecialist.setText(fullName);
+        });
+        fragmentManager.setFragmentResultListener("patientId", this, (requestKey, bundle) -> {
+            reportDto.setPatientId(String.valueOf(bundle.getInt("patientId"))); //TODO Remove parse
+            String fullName = bundle.getString("fullName");
+            reportDto.setPatientFullName(fullName);
+            buttonPatient.setText(fullName);
+        });
 
     }
 
@@ -46,5 +66,99 @@ public class ReportFormFragment extends Fragment {
 
 
         return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //If starts for first time
+        if (reportDto == null) {
+            reportDto = ReportFormFragmentArgs.fromBundle(getArguments()).getReportDto();
+            if (reportDto != null) { //If editing
+                editing = true;
+                fillUpInfoInLayout(reportDto);
+            } else { // If new
+                reportDto = new ReportDto();
+                editing = false;
+            }
+        } else { //If new
+            fillUpInfoInLayout(reportDto);
+        }
+
+        buttonCancel.setOnClickListener(v -> navController.popBackStack());
+        buttonSave.setOnClickListener(v -> {
+            if (allRequiredCampsSet()) save();
+        });
+
+        buttonSpecialist.setOnClickListener(v -> navController.navigate(R.id.action_reportFormFragment_to_selectUserFragment));
+        buttonPatient.setOnClickListener(v -> navController.navigate(R.id.action_reportFormFragment_to_selectPatientFragment));
+    }
+
+    private void save() {
+        if (editing) saveEdited();
+        else saveNew();
+
+    }
+
+    private void saveNew() {
+        final UpdateReportDto updateReportDto = getUpdateReportDto(reportDto);
+    }
+
+    private void saveEdited() {
+    }
+
+    private UpdateReportDto getUpdateReportDto(ReportDto reportDto) {
+        UpdateReportDto updateReportDto = new UpdateReportDto();
+
+        updateReportDto.setActive(switchActive.getShowText());
+        updateReportDto.setDate(reportDto.getDate());//todo Date?
+        updateReportDto.setDiagnosis(reportDto.getDiagnosis());
+        updateReportDto.setDiagnosis(reportDto.getDiagnosis());
+
+
+        //TODO finish this
+        return updateReportDto;
+    }
+
+    private boolean allRequiredCampsSet() {
+        boolean allGood = true;
+        String patientId = reportDto.getPatientId();
+        String specialistId = reportDto.getSpecialistId();
+        CharSequence errorMsg = getText(R.string.must_fill);
+
+        if (patientId == null) {
+            allGood = false;
+            buttonPatient.setError(errorMsg);
+        } if (specialistId == null) {
+            allGood = false;
+            buttonSpecialist.setError(errorMsg);
+        } if (editTextDiagnosis.getText().toString().isEmpty()) {
+            allGood= false; //TODO podemos mandarlo vacio?
+            editTextDiagnosis.setError(errorMsg);
+        } if(editTextObjectives.getText().toString().isEmpty()) {
+            allGood= false;
+            editTextObjectives.setError(errorMsg);
+        }
+
+        return allGood;
+    }
+
+    private void fillUpInfoInLayout(ReportDto reportDto) {
+        String patientName = reportDto.getPatientFullName();
+        String specialistName = reportDto.getPatientFullName();
+        String specialistType = reportDto.getSpecialistType();
+        String diagnosis = reportDto.getDiagnosis();
+        String objectives = reportDto.getObjectives();
+
+        if (patientName != null) buttonPatient.setText(patientName);
+        if (specialistName != null) buttonSpecialist.setText(specialistName);
+        //TODO diagnosis
+        //if (specialistType != null)
+        if (diagnosis != null) editTextDiagnosis.setText(diagnosis);
+        if (objectives != null) editTextObjectives.setText(objectives);
+
+        //TODO active button
+
     }
 }
